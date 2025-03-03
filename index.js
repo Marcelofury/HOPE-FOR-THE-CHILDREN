5481f8a51b881d92d0ccb2a75233646870bf8310
marcelofury <marcelofury13579@gmail.com>
2025-03-03 15:46:28 +0300

    initial plan of the website layout

---
+++ index.js
@@ -0,0 +1,5 @@
+// script.js
+document.addEventListener('DOMContentLoaded', function() {
+    // Add any custom JavaScript here if needed
+    // For example, you might add smooth scrolling or a mobile menu toggle in the future.
+  });
